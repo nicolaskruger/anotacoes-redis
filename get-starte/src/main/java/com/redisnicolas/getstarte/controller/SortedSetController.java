a16bdcab04dfb49e44f11762d00db77c2e7fa71b
nicolas-kruger <nicolas.nkruger@gmail.com>
2021-10-05 16:02:02 -0300

    finish

---
+++ get-starte/src/main/java/com/redisnicolas/getstarte/controller/SortedSetController.java
@@ -0,0 +1,9 @@
+package com.redisnicolas.getstarte.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/sorted-set")
+public class SortedSetController {
+}
